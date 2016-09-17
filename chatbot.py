e08fc058ce6d4c67af5c979bdf311adc12cdde6a
Priyanka Mishra <priyankaormoon@gmail.com>
2016-09-17 19:21:45 -0400

    Final version before submitting

--- chatbot.py
+++ chatbot.py
@@ -1,49 +1,55 @@
 import random
+import requests
 import re
+import json
 # import nltk
 
+URL = "http://ac958344.ngrok.io"
 class Bot(object):
-    def __init__(self, text, first_name, last_name, btc_value, btc_address):
+	def __init__(self):
 		self.andrewID = None
-		self.fbid = fbid
-		self.user = User(first_name,last_name,btc_value,btc_address)
+		self.fbid = None
+		self.user = User()
 
-    def respond_to(self, message):
+	def respond_to(self, message):
+		print "RESPOND TO STARTS"
 		if self.user.question_id != None:
-			return send_answer(message)
+			return self.send_answer(message)
 		listOfProfile = []
 		for word in message:
 			if word.startswith("#"):
 				listOfProfile.append(word)
 		if listOfProfile!=[]:
-			send_hashtags(listOfProfile)
+			self.send_hashtags(listOfProfile)
 			return "Thank you for the information!"
 		if  (self.andrewID==""):
 			return "Welcome to ScottyAsks :) Please enter your AndrewID to begin."
 		if re.search(r'^\$+$|^money$|^btc$|^bitcoin$',message):
-			return handle_btc()
+			return self.handle_btc()
 		if re.search(r'^withdraw$',message):
-			return widthraw()
+			return self.widthraw()
 		match = re.findall(r'address (\w+)',message)
 		if len(match) > 0:
-			return change_address(address)
+			return self.change_address(address)
 		if re.search(r'^#',message):
-			handle_hastag(message)
+			return self.handle_hastag(message)
 		if re.search(r'AskMe|Ask Me|ask me|askme',message):
-			return ask_question()
-		
+			return self.ask_question()
+
 	def send_hashtags(self, hashtags):
-    	"""Send the message text to recipient with id recipient.
-    	"""
-    	r = requests.post(URL + "/users/fb/" + self.fbid + "/update",
-        	params={},
-        	data=json.dumps({
-            	"hashtags": hashtags
-        	}),
-		print r.text
+		"""Send the message text to recipient with id recipient.
+		"""
+		r = requests.post(URL + "/users/fb/" + self.fbid + "/update",
+			params={},
+			data=json.dumps({
+				"hashtags": hashtags
+			}), headers={'Content-type': 'application/json'})
+		print (r.text)
 
 	def handle_btc(self):
-		return "Hi " + self.user.first_name + ". You have " + self.user.bitcoin_value + " BTC in your wallet.\n" + "Would you like to widthdraw or change BTC address?\nTo widthraw, type withdraw\nTo change address, type address <new-address>"
+		self.user.btc_value = "0.1235"
+		self.user.first_name = "Ryan"
+		return "Hi " + self.user.first_name + ". You have " + self.user.btc_value + " BTC in your wallet.\n" + "Would you like to widthdraw or change BTC address?\nTo widthraw, type withdraw\nTo change address, type address <new-address>"
 
 	def withdraw(self):
 		r = rquests.post(URL + "/users/fb/:" + fbid + "/update",
@@ -64,33 +70,33 @@ class Bot(object):
 		return "You have successfully changed your btc address to " + btc_address
 
 	def ask_question(self):
-		r = requests.get(URL + "/users/fb" + self.fbid + "/question")
+		r = requests.get(URL + "/users/fb/" + self.fbid + "/question")
 		question_dict = json.loads(str(r.text))
-		self.user.question_asked = true
-    	self.user.question_id = question_dict["question_id"]
-		self.user.question_text = question_dict["question_text"]
-		return question_dict["question_text"]
-	
-	def send_answer(self,message)
-		r = requests.post(URL + "/users/fb" + fbid + "/question",params = {"question_id" : question_id,"answer_text" = text})
+		self.user.question_id = question_dict["id"]
+		self.user.question_text = question_dict["text"]
+		return self.user.question_text
+
+	def send_answer(self,message):
+		r = requests.post(URL + "/users/fb/" + self.fbid + "/question",
+			params = {"question_id" : self.user.question_id,"answer_text" : self.user.question_text})
 		#if r.status_code != requests.codes.ok:
-        	#print (r.text)
+			#print (r.text)
 		self.user.question_id = None
 		self.user.question_txt = None
-		return r.text
+		return "Thanks!"
 
 class User(object):
-	def __init__(first_name,last_name,btc_value,btc_address):
-		self.first_name = first_name
-		self.last_name = last_name
-		self.btc_value = btc_value
-		self.btc_address = btc_address
+	def __init__(self):
+		self.first_name = None
+		self.last_name = None
+		self.btc_value = None
+		self.btc_address = None
 		self.question_id = None
 		self.question_text = None
-	
+
 
 #     def _format_response(self, content):
-#         def to_unicode(x):
+#         def to_	unicode(x):
 #             if isinstance(x, str):
 #                 return x.decode('utf-8')
 #             return x
--- server.py
+++ server.py
@@ -3,67 +3,133 @@
 import warnings
 import os
 import json
+import requests
 from flask import Flask, request
 
-import chatbot
+from chatbot import *
 import messenger
 
 app = Flask(__name__)
 
 FACEBOOK_TOKEN = os.environ['FACEBOOK_TOKEN']
 bot = dict() # bot as key and fbid as value
-dont_know_id_flag = false
+#know_id_flag = False
 
-@app.route('/', methods=['GET'])
+URL = "http://ac958344.ngrok.io"
+
+@app.route('/verify', methods=['GET'])
 def verify():
-    if request.args.get('hub.verify_token', '') == os.environ['scotty_asks_project_2016']:
+    if request.args.get('hub.verify_token', '') == 'scotty_asks_project_2016':
         return request.args.get('hub.challenge', '')
     else:
-        return 'Error, wrong validation token'
+        return 'Error, wrong validation token ejfherkuf'
 
 def bot_exists(sender_id):
     # checks if the bot exists in the database
     if (sender_id in bot):
-        return true
-    return false
+        return True
+    return False
 
-@app.route('/', methods=['POST'])
+@app.route('/verify', methods=['POST'])
 def webhook():
+    print "DEBUG: WEBHOOK STARTS"
+    #global know_id_flag
+    #global Bot
     payload = request.get_data()
     for sender, message in messenger.messaging_events(payload):
         print ("Incoming from %s: %s") % (sender, message)
-        user_info_dict = check_andrew_ID(sender)
-        curr_user_andrew_ID = user_info_dict["andrewID"]
-        if (bot_exists(sender)):
-            # find existing bot set bot to it 
-            curr_bot = bot(sender)
-            curr_bot.text = message
-        else:
-        # create new bot with info received from get
-            bot[sender].add(Bot(message,sender))
-            curr_bot = bot[sender]
-            curr_bot.user.first_name = user_info_dict["first_name"]
-            curr_bot.user.last_name = user_info_dict["last_name"]
+        sender = str(sender)
 
-        curr_bot.andrewID = curr_user_andrew_ID     
-        curr_bot.user.btc_value = user_info_dict["btc_value"]
-        curr_bot.user.btc_address = user_info_dict["btc_address"]
+        # if bot exists
+        if sender not in bot:
+            bot[sender] = Bot()
 
-        if curr_user_andrew_ID == "" && !dont_know_id_flag:
-            response = "We don't know you. What is your Andrew ID?"
-            dont_know_id_flag = true
-        elif dont_know_id_flag:
-            curr_user_andrew_ID = str(message)
 
-            if(send_unknown_id(sender,curr_user_andrew_ID) == ""):
-                curr_user_andrew_ID = ""
-                dont_know_id_flag = true
+        bot[sender].fbid = sender
+
+
+        data = check_andrew_ID(bot[sender].fbid)
+        if bot[sender].andrewID != None and data["andrewID"] != "":
+            bot[sender].andrewID = data["andrewID"]
+        elif bot[sender].andrewID == None and data["andrewID"] != "":
+            bot[sender].andrewID = data["andrewID"]
+            response = "Welcome to ScottyAsks" + data["andrewID"]
+            bot[sender].user.first_name = data["first_name"]
+            bot[sender].user.last_name = data["last_name"]
+            bot[sender].user.btc_address = data["btc_address"]
+            bot[sender].user.btc_value = data["btc_value"]
+
+        response = "welcome!"
+        if bot[sender].andrewID == None:
+            response = "Welcome to ScottyAsks. What is your Andrew ID?"
+            bot[sender].andrewID = ''
+        elif bot[sender].andrewID == '':
+            data = send_unknown_id(bot[sender].fbid, str(message))
+            if data == "":
+                response = "What is your Andrew ID?"
             else:
-                curr_bot.andrewID = curr_user_andrew_ID
-                user_info_dict["andrewID"] = curr_user_andrew_ID
-                dont_know_id_flag = false
+                print "hitting this"
+                bot[sender].user.andrewID = data["andrewID"]
+                bot[sender].user.first_name = data["first_name"]
+                bot[sender].user.last_name = data["last_name"]
+                bot[sender].user.btc_address = data["btc_address"]
+                bot[sender].user.btc_value = data["btc_value"]
+
         else:
-            response = curr_bot.respond_to(message)
+            response = bot[sender].respond_to(message)
+
+
+
+
+
+
+        # user_info_dict = check_andrew_ID(sender)
+        # curr_user_andrew_ID = user_info_dict["andrewID"]
+        #
+        #
+        #
+        # if (bot_exists(sender)):
+        #     print "DEBUG: BOT EXISTS"
+        #     # find existing bot set bot to it
+        #     curr_bot = bot[sender]
+        #     curr_bot.text = message
+        #     response = curr_bot.respond_to(message)
+        # else: # no existing bot
+        #     bot[sender] = Bot()
+        #     if user_info_dict["andrewID"] != '':
+        #         print "DEBUG: BOT DOESNT EXIST, RECORD EXISTS"
+        #         # create new bot with info received from get
+        #         # only if record already exists in the db
+        #         curr_bot = bot[sender]
+        #         curr_bot.user.first_name = user_info_dict["first_name"]
+        #         curr_bot.user.last_name = user_info_dict["last_name"]
+                # curr_bot.user.btc_address = user_info_dict["btc_address"]
+                # curr_bot.user.btc_value = user_info_dict["btc_value"]
+        #         curr_bot.andrewID = curr_user_andrew_ID
+        #         response = curr_bot.respond_to(message)
+        #     else:
+        #         # no record exists, ask the user for AndrewID
+        #         if bot[sender].andrewID == None:
+        #             print "DEBUG: BOT DOESNT EXIST, RECORD DOESNT EXIST, ASK USER FOR ANDREWID"
+        #             # entering this the first time before the user gave us the andrewID
+                    # response = "We don't know you. What is your Andrew ID?"
+                    # bot[sender].andrewID = ''
+        #         else:
+        #             print "DEBUG: BOT DOESNT EXIST, RECORD DOESNT EXIST, GOT ANDREW ID, ASK DB FOR INFO"
+        #             # user entered andrewID
+        #             curr_user_andrew_ID = str(message)
+        #
+        #             if (send_unknown_id(sender,curr_user_andrew_ID) == ""):
+        #                 # andrewID is wrong
+        #                 bot[sender].andrewID = None
+        #                 #know_id_flag = True
+        #             else:
+        #                 #curr_bot = Bot()
+        #                 bot[sender].andrewID = curr_user_andrew_ID
+        #                 user_info_dict["andrewID"] = curr_user_andrew_ID
+        #                 #know_id_flag = False
+        #         # else:
+        #         #     response = curr_bot.respond_to(message)
 
         print ("Outgoing to %s: %s") % (sender, response)
         messenger.send_message(FACEBOOK_TOKEN, sender, response)
@@ -73,10 +139,11 @@ def webhook():
 
 
 if __name__ == '__main__':
+    print "sadasd"
     app.run(port=5000, debug=True)
 
 def check_andrew_ID(fbid):
-    r = requests.get(URL + "/users/fb" + fbid)
+    r = requests.get(URL + "/users/fb/" + fbid)
     user_info_dict = {};
     if r.text == "":
         return {"andrewID":""}
@@ -87,6 +154,5 @@ def check_andrew_ID(fbid):
     return user_info_dict
 
 def send_unknown_id(fbid,andrewID):
-    r = requests.post(URL + "/users/fb" + fbid,params = {"andrewID":andrewID})
-    if r.status_code != requests.codes.ok:
-        print r.text
+    r = requests.post(URL + "/users/new",params = {"andrewID":andrewID, "fbid":fbid})
+    return json.loads(str(r.text))
