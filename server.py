9dfb056e12df5be27bfc67203135bf3b495823eb
tusharc97 <tusharchetal07@gmail.com>
2016-09-17 14:37:08 -0400

    Routes Added

--- server.py
+++ server.py
@@ -2,6 +2,7 @@
 
 import warnings
 import os
+import json
 from flask import Flask, request
 
 import chatbot
@@ -9,34 +10,83 @@ import messenger
 
 app = Flask(__name__)
 
-FACEBOOK_TOKEN = "EAACuFoER6pQBALD28OyaQcsowXgg5XAgbqj55ONxINleZA20sA87b93NVzwlxjvLUkIPeZB9ZCeDbXZCKZCXSjV5QaLV2gT9lWTdJ7eSiNLB880EyW4inaZCALiXWkZBWDzNgTUjnx7naNhM1OHxtZCScEFJGK7MAaQEZCH1kVPflDQZDZD"
-bot = None
+FACEBOOK_TOKEN = os.environ['FACEBOOK_TOKEN']
+bot = dict() # bot as key and fbid as value
+dont_know_id_flag = false
 
-@app.route('/verify', methods=['GET'])
+@app.route('/', methods=['GET'])
 def verify():
-    if request.args.get('hub.verify_token', '') == "scotty_asks_project_2016":
+    if request.args.get('hub.verify_token', '') == os.environ['scotty_asks_project_2016']:
         return request.args.get('hub.challenge', '')
     else:
         return 'Error, wrong validation token'
 
-@app.route('/verify', methods=['POST'])
+def bot_exists(sender_id):
+    # checks if the bot exists in the database
+    if (sender_id in bot):
+        return true
+    return false
+
+@app.route('/', methods=['POST'])
 def webhook():
     payload = request.get_data()
     for sender, message in messenger.messaging_events(payload):
-        print "Incoming from %s: %s" % (sender, message)
+        print ("Incoming from %s: %s") % (sender, message)
+        user_info_dict = check_andrew_ID(sender)
+        curr_user_andrew_ID = user_info_dict["andrewID"]
+        if (bot_exists(sender)):
+            # find existing bot set bot to it 
+            curr_bot = bot(sender)
+            curr_bot.text = message
+        else:
+        # create new bot with info received from get
+            bot[sender].add(Bot(message,sender))
+            curr_bot = bot[sender]
+            curr_bot.user.first_name = user_info_dict["first_name"]
+            curr_bot.user.last_name = user_info_dict["last_name"]
 
-        response = bot.respond_to(message)
+        curr_bot.andrewID = curr_user_andrew_ID     
+        curr_bot.user.btc_value = user_info_dict["btc_value"]
+        curr_bot.user.btc_address = user_info_dict["btc_address"]
 
-        print "Outgoing to %s: %s" % (sender, response)
+        if curr_user_andrew_ID == "" && !dont_know_id_flag:
+            response = "We don't know you. What is your Andrew ID?"
+            dont_know_id_flag = true
+        elif dont_know_id_flag:
+            curr_user_andrew_ID = str(message)
+
+            if(send_unknown_id(sender,curr_user_andrew_ID) == ""):
+                curr_user_andrew_ID = ""
+                dont_know_id_flag = true
+            else:
+                curr_bot.andrewID = curr_user_andrew_ID
+                user_info_dict["andrewID"] = curr_user_andrew_ID
+                dont_know_id_flag = false
+        else:
+            response = curr_bot.respond_to(message)
+
+        print ("Outgoing to %s: %s") % (sender, response)
         messenger.send_message(FACEBOOK_TOKEN, sender, response)
 
     return "ok"
 
-# if __name__ == '__main__':
-#     # Suppress nltk warnings about not enough data
-#     warnings.filterwarnings('ignore', '.*returning an arbitrary sample.*',)
 
-#     if os.path.exists("corpus.txt"):
-#         bot = chatbot.Bot(open("corpus.txt").read())
 
-#     app.run(port=5000, debug=True)
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
+
+def check_andrew_ID(fbid):
+    r = requests.get(URL + "/users/fb" + fbid)
+    user_info_dict = {};
+    if r.text == "":
+        return {"andrewID":""}
+    else:
+        user_info_dict = json.loads(str(r.text))
+    if r.status_code != requests.codes.ok:
+        print (r.text)
+    return user_info_dict
+
+def send_unknown_id(fbid,andrewID):
+    r = requests.post(URL + "/users/fb" + fbid,params = {"andrewID":andrewID})
+    if r.status_code != requests.codes.ok:
+        print r.text
